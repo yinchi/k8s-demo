e6d0603f1bd95608a2a08318c2b99ef412292da0
Yin-Chi Chan <ycc39@cam.ac.uk>
2024-09-06 11:37:02 +0100

    Fix scripts

--- load_scripts.sh
+++ load_scripts.sh
@@ -6,14 +6,12 @@
 #
 # Add the scripts directory to the system path and sets some environment variables.
 
-echo `prepend_path scripts/`
-export `prepend_path scripts/`
+echo PATH=\"$(echo `prepend_path scripts/`)\"
+export PATH=$(echo `prepend_path scripts/`)
 
 alias popy="poetry run python"
 alias polint="poetry run pylint --rcfile=`git root`/.pylintrc"
 
-alias db_expose="screen -dmS myapp-postgres-portfwd kubectl port-forward -n myapp svc/postgres --address 0.0.0.0 5432:5432 &"
-
+alias db_expose="screen -dmS myapp-postgres-portfwd kubectl port-forward -n myapp svc/postgres0 --address 0.0.0.0 5432:5432 &"
 alias traefik_expose="screen -dmS traefik-dash-portfwd kubectl port-forward -n myapp $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name -n myapp) 9000:9000 &"
-
-alias web_expose="screen -dmS traefik-dash-portfwd kubectl port-forward -n myapp $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name -n myapp) 8000:8000 &"
+alias web_expose="screen -dmS myapp-portfwd kubectl port-forward -n myapp $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name -n myapp) 8000:8000 &"
